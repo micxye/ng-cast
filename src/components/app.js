f4f9fcf04ebf2f44e62c694bf2960aff0b4e5eed
micxye <michaelyeahh@gmail.com>
2018-03-23 20:06:40 -0700

    working on search live videos

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,30 @@ angular.module('video-player')
   // TODO
   templateUrl: "src/templates/app.html",
 
-  controller: function() { 
-    this.videos = window.exampleVideoData;
-    this.currentVideo = window.exampleVideoData[0];
+  controller: function(youTube) { 
+    this.videos = [];
+    this.currentVideo = null;
+
+    this.changeVideos = (videos) => {
+      this.videos = videos;
+      this.currentVideo = videos[0];
+    }
+    this.changeVideos = this.changeVideos.bind(this)
+
+    youTube.search({
+      key:  window.YOUTUBE_API_KEY,
+      max: 5,
+      query: ''
+    }).then(function (response) {
+      this.changeVideos(response.items)
+    }, function (err) {
+      console.log(err);
+    });    
+    //call youtube.search
+    //youtube.search returns a promise
+    //On promise, use .then() function 
+    //Use success callback to set this.videos
+
 
     this.handleClick = (video) => {
       this.currentVideo = video
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,21 @@
 angular.module('video-player')
 .service('youTube', function(){
   // TODO
+  this.search = (options) => {
+    return $http.get('https://www.googleapis.com/youtube/v3/search', {
+      params: {
+        part: 'snippet',
+        key: options.key,
+        maxResults: options.max,
+        q: options.query,
+        videoEmbeddable: 'true',
+        type: 'video'
+      }
+    })
+  }
 });
+
+
+
+
+
